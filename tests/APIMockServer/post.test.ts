cd49dca274de93e031eb882498ee6eb12cc474a3
Michal W. <michal.wagner.mail@gmail.com>
2025-07-24 14:01:42 +0200

    new posts test

---
+++ tests/APIMockServer/post.test.ts
@@ -0,0 +1,26 @@
+import { test , expect, request, APIRequestContext } from "@playwright/test";
+
+test.describe.serial('testing post request', ()=>{
+let apiContext: APIRequestContext;
+const baseURL = 'http://localhost:3000';
+
+    test.beforeAll(async ({})=>{
+        apiContext = await request.newContext({
+            extraHTTPHeaders: {
+                Authorization: 'Bearer my-secret-token',
+            },
+        });
+    });
+
+    test('get all posts and in post id:1 verify content', async ()=>{
+        const response = await apiContext.get(`${baseURL}/posts`);
+        expect(response.status()).toBe(200);
+
+        const data = await response.json();
+        const post = data.find((p: any) => p.id === 1); // V zozname príspevkov (data) hľadáme objekt, ktorého vlastnosť 'id' má hodnotu 1
+
+        expect(post).toBeDefined();
+        expect(post.content).toBe("This is my first post.") // v db.json najdeme pre id.1 content s takymto textom.
+    })
+
+})
