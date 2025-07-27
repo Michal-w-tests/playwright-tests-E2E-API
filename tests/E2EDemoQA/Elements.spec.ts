45c7ba4e340a0058e29fc26e80bdd0bcd8a1dc46
Michal-w-tests <michal.wagner.mail@gmail.com>
2025-07-27 22:56:51 +0200

    last correction today

--- tests/E2EDemoQA/Elements.spec.ts
+++ tests/E2EDemoQA/Elements.spec.ts
@@ -2,11 +2,11 @@ import {test, expect } from '@playwright/test';
 import { NavigationPage } from './pages/NavigationPage';
 import { ElementsPage } from './pages/ElementsPage';
 
-test('Practice form fill out', async ({page})=>{
+test('Playing with elements', async ({page})=>{
     const Navigation = new NavigationPage(page);
     const Elements = new ElementsPage(page);
 
- await test.step('landing on the page and navigating to form', async ()=>{
+ await test.step('navigation to elements section', async ()=>{
     
     await Navigation.LandToUrl();
     await Navigation.navigateToSection('Elements','Please select an item from left to start practice.','Text Box',);
