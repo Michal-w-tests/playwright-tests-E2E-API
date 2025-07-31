3374a9ad4c6ef774dc33521766ac8f038bb5f0cd
Michal-w-tests <michal.wagner.mail@gmail.com>
2025-07-31 11:28:07 +0200

    Widgets accordian

--- tests/E2EDemoQA/Widgets.spec.ts
+++ tests/E2EDemoQA/Widgets.spec.ts
@@ -1,12 +1,16 @@
 import { test,expect } from '@playwright/test'
 import { NavigationPage } from './pages/NavigationPage';
+import { accordionTexts } from './fixtures/accordionTexts';
+import { WidgetsClass } from './pages/Widgets';
 
 test('Widgets', async ({page})=>{
 const Navigation = new NavigationPage(page);
+const Widgets = new WidgetsClass(page);
 
     await test.step('Accordian',async ()=>{
         await Navigation.LandToUrl();
-        await Navigation.navigateToSection('Widgets','Please select an item from left to start practice.','Accordian')
+        await Navigation.navigateToSection('Widgets','Please select an item from left to start practice.','Accordian');
+        await Widgets.Accordian(accordionTexts.WhatisLoremIpsum,accordionTexts.WhereDoesItComeFrom_part1,accordionTexts.WhereDoesItComeFrom_part2,accordionTexts.WhyDoWeUseIt)
     })
 
 })
--- tests/E2EDemoQA/pages/Widgets.ts
+++ tests/E2EDemoQA/pages/Widgets.ts
@@ -1,14 +1,33 @@
 import { test,expect, Page, Locator } from '@playwright/test'
 
-export class Widgets {
+
+export class WidgetsClass {
     readonly page: Page;
 
     constructor(page: Page){
         this.page = page;
     }
 
-    async Accordian(){
-        
+    async Accordian(text1:string,text2a:string,text2b:string,text3:string){
+
+        const Text1 = await this.page.locator('#section1Content > p');
+        await expect(Text1).toContainText(text1)
+
+        const section2 = await this.page.locator('#section2Heading');
+        await section2.click();
+        const Text2 = await this.page.locator('#section2Content > p');
+        await expect(Text2.first()).toContainText(text2a);
+        await expect(Text2.last()).toContainText(text2b)
+
+        const section3 =  await this.page.locator('#section3Heading');
+        await section3.click();
+        const Text3 = await this.page.locator('#section3Content > p');
+        await expect(Text3).toBeVisible();
+        await expect(Text3).toContainText(text3);
+        await expect(Text2.first()).not.toBeVisible();
+        await expect(Text2.last()).not.toBeVisible();
+        await expect(Text1).not.toBeVisible();
+
     }
 
 }
