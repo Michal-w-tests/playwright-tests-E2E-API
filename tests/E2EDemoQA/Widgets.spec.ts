c073be03f5380107c1c32d5f88868efc467f5a79
Michal-w-tests <michal.wagner.mail@gmail.com>
2025-08-03 09:18:17 +0200

    progress bar

--- tests/E2EDemoQA/Widgets.spec.ts
+++ tests/E2EDemoQA/Widgets.spec.ts
@@ -10,7 +10,33 @@ const Widgets = new WidgetsClass(page);
     await test.step('Accordian',async ()=>{
         await Navigation.LandToUrl();
         await Navigation.navigateToSection('Widgets','Please select an item from left to start practice.','Accordian');
-        await Widgets.Accordian(accordionTexts.WhatisLoremIpsum,accordionTexts.WhereDoesItComeFrom_part1,accordionTexts.WhereDoesItComeFrom_part2,accordionTexts.WhyDoWeUseIt)
+        await Widgets.Accordian(accordionTexts.WhatisLoremIpsum,
+                                accordionTexts.WhereDoesItComeFrom_part1,
+                                accordionTexts.WhereDoesItComeFrom_part2,
+                                accordionTexts.WhyDoWeUseIt)
+    });
+
+    await test.step('Auto complete', async()=>{
+        await Navigation.Navigation('Auto Complete');
+        await Widgets.MultipleColorNames('Red','Green');
+        await Widgets.SingleColorName('Black','Green');
+    });
+
+    await test.step('Date Picker',async()=>{
+        await Navigation.Navigation('Date Picker');
+        await Widgets.DatePicker('07/03/2025','06/05/2024');
+        await Widgets.DateAndTimePicker('August 3, 2025 8:00 AM','August 2, 2025 8:30 PM');
+        
+    });
+
+    await test.step('Slider',async()=>{
+        await Navigation.Navigation('Slider');
+        await Widgets.Slider('75')
+    });
+
+    await test.step('Progress Bar',async()=>{
+        await Navigation.Navigation('Progress Bar');
+        await Widgets.ProgressBar()
     })
 
 })
--- tests/E2EDemoQA/pages/Widgets.ts
+++ tests/E2EDemoQA/pages/Widgets.ts
@@ -1,4 +1,5 @@
 import { test,expect, Page, Locator } from '@playwright/test'
+import { Cipheriv } from 'crypto';
 
 
 export class WidgetsClass {
@@ -30,8 +31,85 @@ export class WidgetsClass {
 
         await section3.click();
         await expect(Text3).not.toBeVisible();
+    };
 
+    async MultipleColorNames(color1:string,color2: string){
+        const dropdown = await this.page.locator('#autoCompleteMultipleInput');
+        await dropdown.fill(color1);
+       
+        await this.page.locator('.auto-complete__option', { hasText: color1 }).click();
+        await dropdown.fill(color2);
+        await this.page.locator('.auto-complete__option', { hasText: color2 }).click();
+        
+        const colors = await this.page.locator('.auto-complete__multi-value__label').allTextContents();
+        await expect(colors).toEqual([color1,color2]);
 
+        //removing first (red) color... 
+        await this.page.locator('.css-19bqh2r').nth(0).click()
+        await expect(this.page.locator(`.auto-complete__multi-value__label:has-text("${color1}")`)).toHaveCount(0);
+        
+
+    };
+
+    async SingleColorName(color1:string,color2: string){
+        const dropdown = await this.page.locator('#autoCompleteSingleInput');
+        await dropdown.fill(color1);
+
+        await this.page.locator('.auto-complete__menu', { hasText: color1 }).click();
+        await expect(this.page.locator('.css-1uccc91-singleValue').first()).toHaveText(color1)
+        
+        await dropdown.fill(color2);
+        await this.page.locator('.auto-complete__menu', { hasText: color2 }).click();
+        await expect(this.page.locator('.css-1uccc91-singleValue').first()).toHaveText(color2)
+    };
+
+    async DatePicker(date1:string, date2:string){
+        const date = this.page.locator('#datePickerMonthYearInput');
+        await date.fill(date1);
+        await date.press('Enter');
+        await expect(date).toHaveValue(date1);
+
+        await date.fill(date2);
+        await date.press('Enter');
+        await expect(date).toHaveValue(date2);
+
+        await date.clear();
+        await expect(date).toHaveValue('');
+
+    };
+
+    async DateAndTimePicker(time:string,newTime:string){
+        const DateAndTime = this.page.locator('#dateAndTimePickerInput');
+        await DateAndTime.fill(time)
+        await DateAndTime.press('Enter');
+        await expect(DateAndTime).toHaveValue(time);
+
+        await DateAndTime.fill(newTime);
+        await DateAndTime.press('Enter');
+        await expect(DateAndTime).toHaveValue(newTime)
+
+        await DateAndTime.clear()
+        await expect(DateAndTime).toHaveValue('');
+        await this.page.locator('h1').click();
+    };
+
+    async Slider(value:string){
+        const slider = this.page.locator('input[type="range"]');
+        const sliderValue = this.page.locator('#sliderValue');
+        await slider.fill(value);
+        await expect(sliderValue).toHaveValue(value)
+        
+    };
+
+    async ProgressBar(){
+        const button = this.page.locator('#startStopButton');
+        await button.click();
+        await this.page.waitForTimeout(2000);
+        await button.click();
+        await this.page.waitForTimeout(1000);
+        const valueNow = await this.page.locator('.progress-bar').getAttribute('aria-valuenow');
+        expect(Number(valueNow)).toBeGreaterThan(18);
+        expect(Number(valueNow)).toBeLessThan(22);
     }
 
 }
